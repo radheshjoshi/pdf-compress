2312b8e53ee4a8443b3eb80b82ed5443196e3019
radheshjoshi <radheshjoshi904@gmail.com>
2024-10-02 13:10:21 +0530

    first commit

---
+++ src/main/java/com/example/compress/CompressApplication.java
@@ -0,0 +1,13 @@
+package com.example.compress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompressApplication.class, args);
+	}
+
+}
