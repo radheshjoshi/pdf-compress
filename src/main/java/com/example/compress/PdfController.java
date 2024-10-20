9246e8c0ff57329b9e414bdcb9657151af4ed1d2
radheshjoshi <radheshjoshi904@gmail.com>
2024-10-20 06:22:52 +0530

    add upload document api

--- src/main/java/com/example/compress/PdfController.java
+++ src/main/java/com/example/compress/PdfController.java
@@ -1,7 +1,20 @@
 package com.example.compress;
 
+import jakarta.annotation.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/")
 public class PdfController {
+
+    @RequestMapping(value = "/_upload", method = RequestMethod.POST)
+    public ResponseEntity<Resource> uploadPdf(@RequestParam MultipartFile file){
+
+        return null;
+    }
 }
---
+++ src/main/java/com/example/compress/PdfService.java
@@ -0,0 +1,13 @@
+package com.example.compress;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class PdfService {
+    public ByteArrayResource compressPdf(MultipartFile file) {
+
+        return null;
+    }
+}
